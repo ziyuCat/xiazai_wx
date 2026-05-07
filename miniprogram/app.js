a2a87467b76ae1a0fd8a570c0b850054bbe8195a
yangchen <yangchen6@vanchen.com>
2026-05-07 11:29:54 +0800

    黑色ui

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,8 +2,8 @@ App({
   globalData: {
     // 本地联调可先用文档示例地址，真机和上线环境请改成已配置到小程序后台的 HTTPS 域名。
     // baseUrl: "http://124.221.107.38:8787",
-    baseUrl: "https://www.yyyc1000.site",
-    // baseUrl: "http://127.0.0.1:8787",
+    // baseUrl: "https://www.yyyc1000.site",
+    baseUrl: "http://127.0.0.1:8787",
     currentWork: null,
   },
 });
